1b4e4d8561798d63786be2fee7763ea6ead4a87a
jGr4f <jrodriguezbel@unal.edu.co>
2025-02-19 00:22:14 -0500

    Inicio

---
+++ src/main/CSIA.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package main;
+import view.*;
+
+/**
+ *
+ * @author juanr
+ */
+public class CSIA {
+    public static void main(String[] args) {
+        Login inicio = new Login();
+        inicio.iniciar();
+    }
+}
